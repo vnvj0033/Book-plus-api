0e825735bc7d6d9964a4afb5cb2c972cf8765716
SangYeob <vnvj0033@gmail.com>
2022-11-17 21:21:22 +0900

    Separate parameters

--- kyobobook.js
+++ kyobobook.js
@@ -1,10 +1,25 @@
 const axios = require("axios");
 
+const page = "1";
+const per = "20";
+const period = "002";
+const bsslBksClstCode = "A";
+
 const http = () => {
+  const baseUrl =
+    "https://product.kyobobook.co.kr/api/gw/pub/pdt/best-seller/total?" +
+    "page=" +
+    page +
+    "&per=" +
+    per +
+    "&period=" +
+    period +
+    "&bsslBksClstCode=" +
+    bsslBksClstCode;
+
+  console.log(baseUrl);
   try {
-    return axios.get(
-      "https://product.kyobobook.co.kr/api/gw/pub/pdt/best-seller/total?page=1&per=20&period=002&bsslBksClstCode=A"
-    );
+    return axios.get(baseUrl);
   } catch (error) {
     console.error(error);
   }
@@ -15,17 +30,21 @@ http()
     const data = response.data;
 
     data.data.bestSeller.forEach((element) => {
-
       const book = {
         rank: element.prstRnkn,
-        imageUrl: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/" + element.cmdtCode + ".jpg",
+        imageUrl:
+          "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/" +
+          element.cmdtCode +
+          ".jpg",
         title: element.cmdtName,
         wirter: element.chrcName,
         publisher: element.pbcmName,
-        summary: element.inbukCntt
-      }
+        summary: element.inbukCntt,
+      };
 
-      console.log(book);
+      const json = JSON.stringify(book);
+
+      console.log(json);
     });
   })
   .catch((error) => {
