1b7d29e899a5457fbead53ef287bd5efc667cee5
yoosangyeob <syyoo@joara.com>
2022-11-20 23:16:49 +0900

    fix kyobobook as a function

--- kyobobook.js
+++ kyobobook.js
@@ -1,55 +1,55 @@
 const axios = require("axios");
 
-const express = require("express");
-const app = express();
+loadBookList("1", "2")
 
-const page = "1";
-const per = "20";
-const period = "002";
-const bsslBksClstCode = "A";
+function loadBookList(page, size) {
+  const period = "002";
+  const bsslBksClstCode = "A";
 
-const http = () => {
-  const baseUrl =
-    "https://product.kyobobook.co.kr/api/gw/pub/pdt/best-seller/total?" +
-    "page=" +
-    page +
-    "&per=" +
-    per +
-    "&period=" +
-    period +
-    "&bsslBksClstCode=" +
-    bsslBksClstCode;
 
-  console.log(baseUrl);
-  try {
-    return axios.get(baseUrl);
-  } catch (error) {
-    console.error(error);
-  }
-};
 
-http()
-  .then((response) => {
+  const http = () => {
+    const baseUrl = "https://product.kyobobook.co.kr/api/gw/pub/pdt/best-seller/total";
+
+    try {
+      return axios.get(baseUrl, {
+        params: {
+          page: page,
+          per: size,
+          period: period,
+          bsslBksClstCode: bsslBksClstCode,
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  http().then((response) => {
     const data = response.data;
-
     data.data.bestSeller.forEach((element) => {
-      const book = {
-        rank: element.prstRnkn,
-        imageUrl:
-          "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/" +
-          element.cmdtCode +
-          ".jpg",
-        title: element.cmdtName,
-        wirter: element.chrcName,
-        publisher: element.pbcmName,
-        summary: element.inbukCntt,
-      };
+      const book = parserBook(element);
 
       const json = JSON.stringify(book);
 
       console.log(json);
     });
-  })
-  .catch((error) => {
+  }).catch((error) => {
     console.error(error);
   });
+}
+
+
+function parserBook(element) {
+  return {
+    rank: element.prstRnkn,
+    imageUrl:
+      "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/" +
+      element.cmdtCode +
+      ".jpg",
+    title: element.cmdtName,
+    wirter: element.chrcName,
+    publisher: element.pbcmName,
+    summary: element.inbukCntt,
+  };
+}
