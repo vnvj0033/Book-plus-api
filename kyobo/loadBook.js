2811ae457232621bebd7416c04570fe5bb956011
SangYeob <vnvj0033@gmail.com>
2022-11-22 21:10:48 +0900

    remove parser function in loader

--- kyobo/loadBook.js
+++ kyobo/loadBook.js
@@ -16,17 +16,3 @@ module.exports = function loadBook(page, size) {
     },
   });
 };
-
-function parserBook(element) {
-  return {
-    rank: element.prstRnkn,
-    imageUrl:
-      "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/" +
-      element.cmdtCode +
-      ".jpg",
-    title: element.cmdtName,
-    wirter: element.chrcName,
-    publisher: element.pbcmName,
-    summary: element.inbukCntt,
-  };
-}
