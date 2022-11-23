afc09850dd8746dd1757109f11c88d84d8e82251
SangYeob <vnvj0033@gmail.com>
2022-11-23 19:20:09 +0900

    add database mysql

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const kyobo = require("./kyobo/loadBook.js");
 const parser = require("./kyobo/parser.js");
+const sql = require("./database/testdb.js");
+const connection = sql.init();
 
-kyobo("1", "1")
-  .then((response) => {
-    const data = response.data;
-    data.data.bestSeller.forEach((element) => {
-      const book = parser(element);
+sql.db_open(connection);
 
-      console.log(book);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+connection.query("SELECT * FROM  USERS", function (error, results, fields) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+});
+
+// kyobo("1", "1")
+//   .then((response) => {
+//     const data = response.data;
+//     data.data.bestSeller.forEach((element) => {
+//       const book = parser(element);
+
+//       console.log(book);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
