6e9d9bcb591145a8fd5c7f6c139cbad278e93f9d
yoosangyeob <syyoo@joara.com>
2022-11-24 17:32:56 +0900

    feat create table of book

--- index.js
+++ index.js
@@ -6,22 +6,35 @@ models.sequelize
   .sync()
   .then(() => {
     console.log("✓ DB 연결 성공");
-
-    kyobo("1", "1")
-      .then((response) => {
-        const data = response.data;
-        data.data.bestSeller.forEach((element) => {
-          const book = parser(element);
-
-          console.log(book);
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    start()
   })
   .catch(function (err) {
     console.error(err);
     console.log("✗ DB 연결 에러");
     process.exit();
   });
+
+
+function start() {
+  kyobo("1", "1")
+    .then((response) => {
+      const data = response.data;
+      data.data.bestSeller.forEach((element) => {
+        const book = parser(element);
+
+        models.Book.create({
+          title: book.title,
+          rank: book.rank,
+          imageUrl: book.imageUrl,
+          wirter: book.wirter,
+          publisher: book.publisher,
+          summary: book.summary
+        })
+
+        console.log(book);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
