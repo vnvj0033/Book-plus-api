2f3cd23f771fa38166e85580392cc03a827d13a7
SangYeob <vnvj0033@gmail.com>
2022-11-30 20:39:43 +0900

    add userFavoriteGenre table

--- scheduler/bookCron.js
+++ scheduler/bookCron.js
@@ -1,44 +1,46 @@
-const cron = require('node-cron');
+const cron = require("node-cron");
 const kyobo = require("../kyobo/loadBook.js");
 
 exports = function startCron() {
+  const task = cron.schedule(
+    "0 0 0 * *",
+    function () {
+      console.log("매 시 마다 작업 실행");
+      start();
+    },
+    {
+      scheduled: false,
+    }
+  );
 
-    const task = cron.schedule('0 0 0 * *', function () {
-        console.log('매 시 마다 작업 실행');
-        start()
-    }, {
-        scheduled: false
-    });
-
-    task.start();
-}
-
+  task.start();
+};
 
 function start() {
-    kyobo("1", "1")
-        .then((response) => {
-            const data = response.data;
-            data.data.bestSeller.forEach((element) => {
-                const book = parser(element);
+  kyobo("1", "1")
+    .then((response) => {
+      const data = response.data;
+      data.data.bestSeller.forEach((element) => {
+        const book = parser(element);
 
-                models.Book.create({
-                    title: book.title,
-                    rank: book.rank,
-                    imageUrl: book.imageUrl,
-                    wirter: book.wirter,
-                    publisher: book.publisher,
-                    summary: book.summary,
-                });
-
-                console.log(book.title + " 저장 성공");
-            });
-        })
-        .catch((error) => {
-            console.error(error);
+        models.Book.create({
+          title: book.title,
+          rank: book.rank,
+          imageUrl: book.imageUrl,
+          wirter: book.wirter,
+          publisher: book.publisher,
+          summary: book.summary,
         });
 
-    models.User.create({
-        deviceId: "deviceId",
-        bookTitle: "bookTitle",
+        console.log(book.title + " 저장 성공");
+      });
+    })
+    .catch((error) => {
+      console.error(error);
     });
+
+  models.User.create({
+    deviceId: "deviceId",
+    bookTitle: "bookTitle",
+  });
 }
