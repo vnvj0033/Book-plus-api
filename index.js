7f92d2aaf6881863809821e88f1bc780d8192fa9
yoosangyeob <syyoo@joara.com>
2023-02-24 22:21:06 +0900

    test get /books

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const parser = require("./kyobo/parser.js");
 const models = require("./models");
-const bookDao = require("./dao/bookDao.ts");
+// const bookDao = require("./dao/bookDao.ts");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -9,19 +9,42 @@ app.use(express.json());
 
 app.get("/books", (req, res) => {
   const title = req.query.title;
+  console.log('get book')
+  console.log(req.query)
 
-  bookDao.findBook(title, (books) => {
-    res.send({ books });
-  });
+  let data = [
+    {
+      title: 'title1',
+      wirter: 'wirter1',
+      summary: 'this is book of bookplus test wow!'
+    },
+    {
+      title: 'title2',
+      wirter: 'wirter2',
+      summary: 'this is book of bookplus test wow! 22'
+    },
+    {
+      title: 'title3',
+      wirter: 'wirter3',
+      summary: 'this is book of bookplus test wow! 33'
+    },
+  ]
+
+  res.send(data)
+  // bookDao.findBook(title, (books) => {
+  //   res.send({ books });
+  // });
 });
 
-app.post("/books", (req, res) => {
-  const books = req.body.books;
 
-  books.forEach((book) => {
-    bookDao.saveBook(book);
-  });
-  res.send({ result: "ok" });
+app.post("/books", (req, res) => {
+
+  // const books = req.body.books;
+
+  // books.forEach((book) => {
+  //   bookDao.saveBook(book);
+  // });
+  // res.send({ result: "ok" });
 });
 
 app.listen(3000, () => {
