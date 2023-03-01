b35462cad875ab2e5f7f191630e20da30696d8bb
SangYeob <vnvj0033@gmail.com>
2023-03-01 12:25:09 +0900

    fixed kyobo loadBook

--- kyobo/loadBook.js
+++ kyobo/loadBook.js
@@ -1,18 +1,20 @@
 const axios = require("axios");
 
-module.exports = function loadBook(page, size) {
+module.exports = function loadBook(page, size, callback) {
   const period = "002";
   const bsslBksClstCode = "A";
 
   const baseUrl =
     "https://product.kyobobook.co.kr/api/gw/pub/pdt/best-seller/total";
 
-  return axios.get(baseUrl, {
-    params: {
-      page: page,
-      per: size,
-      period: period,
-      bsslBksClstCode: bsslBksClstCode,
-    },
-  });
+  axios
+    .get(baseUrl, {
+      params: {
+        page: page,
+        per: size,
+        period: period,
+        bsslBksClstCode: bsslBksClstCode,
+      },
+    })
+    .then((response) => callback(response));
 };
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const add = require("./dao/book/addBook.js");
 const getAll = require("./dao/book/getAllBook.js");
 const getBook = require("./dao/book/getBook.js");
+const loadBook = require("./kyobo/loadBook.js");
 
-getBook("booitle", (data) => {
-  console.log(data);
+loadBook(0, 10, (result) => {
+  console.log(result.data.data.bestSeller);
 });
