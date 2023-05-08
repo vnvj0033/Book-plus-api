a9474168cb70bea16362001662fcda63e0887e84
yusang-yeob <vnvj0033@gmail.com>
2023-05-08 21:42:17 +0900

    added model Book

---
+++ object/Book.js
@@ -0,0 +1,9 @@
+class Book {
+    constructor(img_url, title, wirter, summary, genre) {
+        this.img_url = img_url
+        this.title = title
+        this.wirter = wirter
+        this.summary = summary
+        this.genre = genre
+    }
+}
