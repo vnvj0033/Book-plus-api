c24a5702df4c9616b643453c72591e40637978eb
yusang-yeob <vnvj0033@gmail.com>
2023-05-10 19:33:11 +0900

    line clean up

--- scheduler/bookCron.js
+++ scheduler/bookCron.js
@@ -18,25 +18,24 @@ exports = function startCron() {
 
 function start() {
   kyobo("1", "1").then((response) => {
-      const data = response.data;
-      data.data.bestSeller.forEach((element) => {
-        const book = parser(element);
+    const data = response.data;
+    data.data.bestSeller.forEach((element) => {
+      const book = parser(element);
 
-        models.Book.create({
-          title: book.title,
-          rank: book.rank,
-          imageUrl: book.imageUrl,
-          wirter: book.wirter,
-          publisher: book.publisher,
-          summary: book.summary,
-        });
-
-        console.log(book.title + " 저장 성공");
+      models.Book.create({
+        title: book.title,
+        rank: book.rank,
+        imageUrl: book.imageUrl,
+        wirter: book.wirter,
+        publisher: book.publisher,
+        summary: book.summary,
       });
-    })
-    .catch((error) => {
-      console.error(error);
+
+      console.log(book.title + " 저장 성공");
     });
+  }).catch((error) => {
+    console.error(error);
+  });
 
   models.User.create({
     deviceId: "deviceId",
