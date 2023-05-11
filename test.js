3e3ead516d771e094dc618f8be33f9bd3c15678d
yusang-yeob <vnvj0033@gmail.com>
2023-05-11 23:12:43 +0900

    fixed test

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
 const parser = require("./datasorce/axios/kyobo/parser");
 const loadBook = require("./datasorce/axios/kyobo/loadBook")
 
-loadBook("1", "1").then((response) => {
-    const data = response.data;
-    data.data.bestSeller.forEach((element) => {
-      const book = parser(element);
-
-      console.log(book)
-    })
+loadBook("1", "1", (response) => {
+    response.array.forEach(element => {
+        const book = parser(element);
+        console.log(book)
+    });
 })
