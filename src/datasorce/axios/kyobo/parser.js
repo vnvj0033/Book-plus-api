cd024e0e8c966652c4ad800f5f28cae8b3a60e67
yusang-yeob <vnvj0033@gmail.com>
2023-05-13 14:44:22 +0900

    fixed paser

--- src/datasorce/axios/kyobo/parser.js
+++ src/datasorce/axios/kyobo/parser.js
@@ -1,12 +1,10 @@
-const Book = require("../../../object/Book" )
-
 module.exports = function parserBook(element) {
-  const book = new Book(
-    "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/" + element.cmdtCode + ".jpg",
-    element.cmdtName,
-    element.chrcName,
-    element.pbcmName,
-    element.inbukCntt
-  )
+  const book = {
+    'img_url':"https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/" + element.cmdtCode + ".jpg",
+    'title':element.cmdtName,
+    'wirter':element.chrcName,
+    'publish':element.pbcmName,
+    'summery':element.inbukCntt
+  }
   return book
 };
