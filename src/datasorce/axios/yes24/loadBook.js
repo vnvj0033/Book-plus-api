85ab8b103d6478af97f825699b8df14b7129e9d9
SangYeob <vnvj0033@gmail.com>
2023-05-29 22:15:21 +0900

    fixed load book of yes24

--- src/datasorce/axios/yes24/loadBook.js
+++ src/datasorce/axios/yes24/loadBook.js
@@ -1,50 +1,58 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const iconv = require('iconv-lite');
-const genres = require('./gener')
+const iconv = require("iconv-lite");
+const genres = require("./gener");
 
 module.exports = function loadBook(page, size, genre, callback) {
-
-  const code = genres.filter(data => {
-    return data.name == genre
-  })[0].code
+  const code = genres.filter((data) => {
+    return data.name == genre;
+  })[0].code;
 
   axios({
-    url: 'http://www.yes24.com/24/category/bestSeller?CategoryNumber=' + code,
-    method: 'GET',
-    responseType: 'arraybuffer'
-  }).then(response => {
-    const content = iconv.decode(response.data, 'EUC-KR');
-    const $ = cheerio.load(content);
+    url: "http://www.yes24.com/24/category/bestSeller?CategoryNumber=" + code,
+    method: "GET",
+    responseType: "arraybuffer",
+  })
+    .then((response) => {
+      const content = iconv.decode(response.data, "EUC-KR");
+      const $ = cheerio.load(content);
 
-    const list = new Array
-    // 1위~40위까지의 책들에 대한 selector
-    const startPage = page * size - size
+      const list = new Array();
+      // 1위~40위까지의 책들에 대한 selector
+      const startPage = page * size - size;
 
-    const booksSelector = '#category_layout > tbody > tr';
-    $(booksSelector).each((i, elem) => {
-      if (startPage <= i && i <= size + startPage - 1) {
-        const title = $(elem).find('td.goodsTxtInfo > p:nth-child(1) > a:nth-child(1)').text();
-        const rank = i + 1
-        const image_url = $(elem).find('td.image > div > a:nth-child(1) > img').attr('src');
-        const writer = $(elem).find('td.goodsTxtInfo > div > a:nth-child(1)').text();
-        const publisher = $(elem).find('td.goodsTxtInfo > div > a:nth-child(2)').text();
-        const summary = $(elem).find('td:nth-child(2) > p').text();
-        list.push({
-          'platform': 'yes24',
-          'rank': rank,
-          'title': title,
-          'image_url': image_url,
-          'writer': writer,
-          'publisher': publisher,
-          'summary': summary
-        })
-      }
+      const booksSelector = "#category_layout > tbody > tr";
+      $(booksSelector).each((i, elem) => {
+        if (startPage <= i && i <= 2 * (size + startPage - 1) && i % 2 == 0) {
+          const title = $(elem)
+            .find("td.goodsTxtInfo > p:nth-child(1) > a:nth-child(1)")
+            .text();
+          const rank = i + 1 - i / 2;
+          const image_url = $(elem)
+            .find("td.image > div > a:nth-child(1) > img")
+            .attr("src");
+          const writer = $(elem)
+            .find("td.goodsTxtInfo > div > a:nth-child(1)")
+            .text();
+          const publisher = $(elem)
+            .find("td.goodsTxtInfo > div > a:nth-child(2)")
+            .text();
+          const summary = $(elem).find("td:nth-child(2) > p").text();
+          list.push({
+            platform: "yes24",
+            rank: rank,
+            title: title,
+            image_url: image_url,
+            writer: writer,
+            publisher: publisher,
+            summary: summary,
+          });
+        }
+      });
+
+      callback(list);
+    })
+    .catch((err) => {
+      console.error(err);
     });
-
-    callback(list)
-  }).catch(err => {
-    console.error(err);
-  });
-
-}
+};
--- src/test.js
+++ src/test.js
@@ -1,15 +1,15 @@
-const loadBook = require("./datasorce/axios/kyobo/loadBook")
-const bookNetLoader = require("./datasorce/axios/bookNetLoader")
-const yes24BookLoader = require("./datasorce/axios/yes24/loadBook")
-const kyoboBookLoader = require("./datasorce/axios/kyobo/loadBook")
-const aladinBookLoader = require("./datasorce/axios/aladin/loadBook")
-const bookDao = require("./datasorce/dao/bookDao")
-const models = require("./datasorce/models")
+const loadBook = require("./datasorce/axios/kyobo/loadBook");
+const bookNetLoader = require("./datasorce/axios/bookNetLoader");
+const yes24BookLoader = require("./datasorce/axios/yes24/loadBook");
+const kyoboBookLoader = require("./datasorce/axios/kyobo/loadBook");
+const aladinBookLoader = require("./datasorce/axios/aladin/loadBook");
+const bookDao = require("./datasorce/dao/bookDao");
+const models = require("./datasorce/models");
 
-const bookLoader = new bookNetLoader()
-const dao = new bookDao()
+const bookLoader = new bookNetLoader();
+const dao = new bookDao();
 
-
-yes24BookLoader(1, 10, '종교개신교', (list) => {
-    console.log(list)
-})
+const genres = require("./datasorce/axios/yes24/gener");
+yes24BookLoader(1, 10, genres[1].name, (list) => {
+  console.log(list);
+});
